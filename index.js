45f53787473be54d1ed20f35a431021a0cc73d17
Jay <chunsaa@gmail.com>
2020-03-02 23:12:13 -0500

    add post route to accept image

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 const users = require('./routes/users');
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+
 
 const app = express();
 
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', users);
@@ -23,6 +26,23 @@ app.get('/api/getList', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+
+app.post('/upload', (req, res) => {
+  if (req.files === null) return res.status(400).json({ msg: 'No file was uploaded' });
+  if (req.files) {
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/uploads/${file.name}`, err => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      }
+
+      res.json({ fileName: file.name, filePath: `/uploads/${file.name}` })
+    })
+  }
+  
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port);
